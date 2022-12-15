ab86e6af8c8d80cd5c572ae314679f29cf6fb947
nyr <kauzoo@web.de>
2022-12-15 18:55:00 +0100

    Porting WIP Files

---
+++ SafeGetComponentPackage/Nyr/Util/SafeGetComponent/Tests/GetterTester.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using static Nyr.UnityDev.Util.GetComponentSafe;
+
+namespace Nyr.UnityDev.Util.Tests
+{
+    public class GetterTester : SafeComponentGetters
+    {
+        public Transform trans;
+        public Collider coll;
+        public Bounds bounds;
+
+        [ContextMenu("GetTransform")]
+        public void GetTransform()
+        {
+            // Inherited version
+            coll = SafeNullableGetComponent<Collider>();
+            bounds = (Bounds) coll?.bounds;
+
+            // Standard unity way
+            // trans = GetComponent<Transform>();
+        }
+
+        [ContextMenu("GetChildTransform")]
+        public void GetChildTransform()
+        {
+            if (TryGetComponentInChildren<Collider>(out var transComp))
+            {
+                coll = transComp;
+            }
+        }
+
+        [ContextMenu("ClearTransform")]
+        public void ClearTransform()
+        {
+            trans = null;
+        }
+    }
+
+    public class GetterTester2 : MonoBehaviour
+    {
+        public Transform trans;
+
+        public void GetTransform()
+        {
+            // Extension method syntax
+            trans = this.SafeGetComponent<Transform>();
+        }
+    }
+}
